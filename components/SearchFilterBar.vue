0de1adefca72c9f1e356c3e8aa278c62bdd2d1fa
Narain Singaram <narainsingaram7@gmail.com>
2023-09-29 18:01:33 -0400

    Some Changes I had Before

--- components/SearchFilterBar.vue
+++ components/SearchFilterBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search-filter-bar flex flex-col items-center">
-    <div class="search-input-wrapper relative w-80 my-4">
+    <div class="search-input-wrapper relative w-96 mx-auto my-4">
       <input
         id="search"
         v-model="searchQuery"
@@ -11,13 +11,13 @@
         placeholder="Search for Businesses"
         class="input bg-neutral-focus transition w-full max-w-xs rounded-full pl-4 pr-10"
       />
-      <div class="input-icon absolute inset-y-0 right-3 flex items-center">
+      <div class="input-icon absolute inset-y-0 right-0 flex items-center">
         <svg
           fill="none"
           stroke="currentColor"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
           viewBox="0 0 24 24"
           height="1em"
           width="1em"
@@ -28,7 +28,7 @@
         </svg>
       </div>
     </div>
-    <div v-if="showNoResultsMessage" class="w-80 my-4 alert alert-error">
+    <div v-if="showNoResultsMessage" class="w-96 mx-auto my-4 alert alert-error">
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
       <span>
         No Results Found.
