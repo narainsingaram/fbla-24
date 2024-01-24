5e9d2da6997c753acb2a884979c40fe51edbe24d
Narain Singaram <narainsingaram7@gmail.com>
2024-01-24 10:10:10 -0500

    made some changes

---
+++ config.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7sGBgz70e3BY-u6PtZemvUqeWdKldgzg",
+  authDomain: "nexus-connect-3c581.firebaseapp.com",
+  databaseURL: "https://nexus-connect-3c581-default-rtdb.firebaseio.com",
+  projectId: "nexus-connect-3c581",
+  storageBucket: "nexus-connect-3c581.appspot.com",
+  messagingSenderId: "722221540651",
+  appId: "1:722221540651:web:20e11062f4332f1e50516e",
+  measurementId: "G-QT17FE1SPL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
