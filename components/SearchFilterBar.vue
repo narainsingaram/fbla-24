90624a534379501cf2aff0f4adff3c0818c04cda
Narain Singaram <narainsingaram7@gmail.com>
2024-02-03 11:16:47 -0500

    added voice search, fixed map search bug

--- components/SearchFilterBar.vue
+++ components/SearchFilterBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="search-filter-bar flex flex-col items-center mx-8"> <!-- Added w-full to make the container span the entire page -->
-    <div class="search-input-wrapper relative w-9/12 my-4"> <!-- Changed w-80 to w-full -->
+  <div class="search-filter-bar flex flex-col items-center mx-8">
+    <div class="search-input-wrapper relative w-9/12 my-4">
       <input
         id="search"
         v-model="searchQuery"
@@ -14,6 +14,12 @@
       <div class="input-icon absolute inset-y-0 right-0 flex items-center mr-4">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
       </div>
+      <button @click="startVoiceSearch" 
+        class="input-icon absolute inset-y-0 right-10 flex items-center mr-4 voice-search-button">
+        <svg height="21px" version="1.1" viewBox="0 0 14 21" width="14px" xmlns="http://www.w3.org/2000/svg" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns:xlink="http://www.w3.org/1999/xlink"><title/><desc/><defs/><g fill="none" fill-rule="evenodd" id="Page-1" stroke="none" stroke-width="1"><g fill="#000000" id="Icons-AV" transform="translate(-3.000000, -43.000000)">
+          <g id="mic" transform="translate(3.000000, 43.500000)">
+            <path d="M7,12 C8.7,12 10,10.7 10,9 L10,3 C10,1.3 8.7,0 7,0 C5.3,0 4,1.3 4,3 L4,9 C4,10.7 5.3,12 7,12 L7,12 Z M12.3,9 C12.3,12 9.8,14.1 7,14.1 C4.2,14.1 1.7,12 1.7,9 L0,9 C0,12.4 2.7,15.2 6,15.7 L6,19 L8,19 L8,15.7 C11.3,15.2 14,12.4 14,9 L12.3,9 L12.3,9 Z" id="Shape"/></g></g></g></svg>      
+      </button>
     </div>
     <div v-if="showNoResultsMessage" class="w-96 mx-auto my-4 alert alert-error">
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
@@ -26,7 +32,7 @@
 </template>
 
 <script>
-import partnersData from '@/assets/partners.json';
+import partnersData from '@/backup.json';
 
 export default {
   data() {
@@ -70,7 +76,28 @@ export default {
     reloadPage() {
       // Reload the page
       location.reload();
-    }
+    },
+    startVoiceSearch() {
+  const recognition = new webkitSpeechRecognition();
+  recognition.lang = 'en-US';
+
+  recognition.onresult = (event) => {
+    const voiceInput = event.results[0][0].transcript;
+    this.searchQuery = voiceInput;
+    this.handleSearch();
+    this.hideSuggestions();
+  };
+
+  recognition.onerror = (event) => {
+    console.error('Speech recognition error:', event.error);
+  };
+
+  recognition.onnomatch = (event) => {
+    console.log('No match found for speech input:', event);
+  };
+
+  recognition.start();
+}
   }
 };
 </script>
